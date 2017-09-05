329a71331d2b2a617bb9c56bae5490da7d5843cd
Giriyelamanchili <yelamanchiligirigiri19@gmail.com>
2017-09-05 14:32:44 +0530

    Update script1.sh

--- script1.sh
+++ script1.sh
@@ -16,23 +16,25 @@ git config --global user.email "andranagendra720@gmail.com"
 
 git remote add nag https://github.com/andranagendra/filee.git
 
-#wget --no-cookies --no-check-certificate --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie" "http://download.oracle.com/otn-pub/java/jdk/8u131-b11/d54c1d3a095b4ff2b6607d096fa80163/jdk-8u131-linux-x64.tar.gz" 
+cd  /opt
 
-#tar xzf  jdk-8u131-linux-x64.tar.gz
+wget --no-cookies --no-check-certificate --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie" "http://download.oracle.com/otn-pub/java/jdk/8u131-b11/d54c1d3a095b4ff2b6607d096fa80163/jdk-8u131-linux-x64.tar.gz" 
 
-#cd  /opt/jdk1.8.0_131/
+tar xzf  jdk-8u131-linux-x64.tar.gz
 
-#alternatives --install /usr/bin/java java /opt/jdk1.8.0_131/bin/java 2
-#alternatives --config java
+cd  /opt/jdk1.8.0_131/
+
+alternatives --install /usr/bin/java java /opt/jdk1.8.0_131/bin/java 2
+alternatives --config java
 #alternatives --install /usr/bin/jar jar /opt/jdk1.8.0_131/bin/jar 2
 #alternatives --install /usr/bin/javac javac /opt/jdk1.8.0_131/bin/javac 2
 #alternatives --set jar /opt/jdk1.8.0_131/bin/jar
 #alternatives --set javac /opt/jdk1.8.0_131/bin/javac
 #java -version
 
-#export JRE_HOME=/opt/jdk1.8.0_131/jre
-#export JAVA_HOME=/opt/jdk1.8.0_131
-#export PATH=$PATH:/opt/jdk1.8.0_131/bin:/opt/jdk1.8.0_131/jre/bin
+export JRE_HOME=/opt/jdk1.8.0_131/jre
+export JAVA_HOME=/opt/jdk1.8.0_131
+export PATH=$PATH:/opt/jdk1.8.0_131/bin:/opt/jdk1.8.0_131/jre/bin
 
 
 # To set up for jenkins 
